c09720d7f4b8aee5e2a7078906267c06e72cdd21
ARYPROGRAMMER <arya.2023ug1104@iiitranchi.ac.in>
2024-11-28 03:36:10 +0530

    websocket implemented phase 1

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,23 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import Routes from "./routes/index.js";
 import { createServer } from "http";
+import { Server } from "socket.io";
 
 import cors from "cors";
+import { setupSocket } from "./socket.js";
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+setupSocket(io);
+export {io};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,7 +27,6 @@ app.get("/", (req: Request, res: Response) => {
   return res.send("It's working Guys 🙌");
 });
 
-app.use('/api', Routes);
-
+app.use("/api", Routes);
 
 server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
